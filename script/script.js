43e5c451d6482ad7f29a2015ed64e68370afe20d
Matheus Mello <matheus_usagui@hotmail.com>
2022-10-17 19:42:20 -0300

    script-galeria/faq/anime

--- script/script.js
+++ script/script.js
@@ -3,13 +3,10 @@ const links = document.querySelectorAll(".header-menu a");
 function adicionarAtivo(link) {
   if (location.href.includes(link.href)) {
     link.classList.add("ativo");
-    console.log("Menu Ativo no Header = True");
   }
 }
 links.forEach(adicionarAtivo);
 //
-
-
 
 // PARAMETROS DE URL
 const parametros = new URLSearchParams(location.search);
@@ -20,25 +17,48 @@ function ativarProduto(param) {
   if (element) {
     element.checked = true;
   }
-  console.log('Parametros de URL = True');
 }
 //
 
+// PERGUNTAS ATIVO/INATIVO
+const perguntas_button = document.querySelectorAll(".lista-de-perguntas button");
+perguntas_button.forEach(eventosPerguntas);
 
+function eventosPerguntas(perg) {
+  perg.addEventListener("click", ativarPergunta);
+}
 
+function ativarPergunta(event) {
+  const pergunta_button = event.currentTarget;
+  const controls = pergunta_button.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
 
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta_button.setAttribute("aria-expanded", ativa);
+}
+//
 
-// const bicicletas = Array.from(document.querySelectorAll(".bicicletas-imagens img"));
-// bicicletas.forEach(mudarPrincipal);
-// bicicletas.shift();
+// GALERIA BICICLETAS
+const bicicletas = Array.from(document.querySelectorAll(".bicicletas-imagens img"));
+const bicicletas_div = document.querySelector(".bicicletas-imagens");
+bicicletas.forEach(mudarPrincipal);
 
-// function mudarPrincipal(imagem) {
-//   imagem.addEventListener("click", mudarImagem);
-// }
+function mudarPrincipal(imagem) {
+  imagem.addEventListener("click", mudarImagem);
+}
 
-// function mudarImagem() {
-//   const bicicletas_principal = document.querySelector(".bicicletas-imagens img");
-//   const target = event.currentTarget;
-//   bicicletas_principal.src = target.src;
-//   console.log('Teste Galeria');
-// }
+function mudarImagem(event) {
+  const img_target = event.currentTarget;
+  const media = matchMedia("(min-width: 920px)").matches;
+  if (media) {
+    bicicletas_div.prepend(img_target);
+  }
+}
+//
+
+// ANIMAÇÃO (PLUGIN)
+if(window.SimpleAnime){
+new SimpleAnime();
+}
+//
