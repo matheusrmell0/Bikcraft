6a2ac170e1cd4df2ac403bc5967c1389b5a48dc1
Matheus Mello <matheus_usagui@hotmail.com>
2022-10-18 18:48:52 -0300

    scrip-animacao-manual

--- script/script.js
+++ script/script.js
@@ -11,7 +11,6 @@ links.forEach(adicionarAtivo);
 // PARAMETROS DE URL
 const parametros = new URLSearchParams(location.search);
 parametros.forEach(ativarProduto);
-
 function ativarProduto(param) {
   const element = document.getElementById(param);
   if (element) {
@@ -67,4 +66,88 @@ if (window.SimpleAnime) {
 }
 //
 
+// REFRESH TIME
+if (document.getElementById("index")) {
+  setTime(0);
+  var updateDelay = 1;
+
+  function setTime() {
+    var date = new Date();
+    timestamp = date.getTime();
+  }
+
+  function updateLastRefreshTime() {
+    var currentDate = new Date();
+    var currentTime = currentDate.getTime();
+    var timeSince = Math.floor((currentTime - timestamp) / 1000);
+    let timeStampDescription = "";
+
+    timeStampDescription = timeSince;
+    const refresh = timeStampDescription;
+    if (refresh > 5) {
+      bikelist_ul.classList.add("opacity-1");
+    }
+    if (refresh > 10) {
+      document
+        .querySelector(".tecnologia-imagem")
+        .classList.add("opacity-1-left");
+    }
+    if (refresh > 15) {
+      document.querySelector(".parceiros h2").classList.add("opacity-1-right");
+    }
+  }
+  setInterval(updateLastRefreshTime, updateDelay);
+  //
+
+  // ANIMAÇÃO SCRIPT MANUAL
+  window.addEventListener("scroll", scrolling);
+  const bikelist_ul = document.querySelector(".ul-bike");
+  const ouro_pag = document.getElementById("seguros-items-ouroid");
+  const prata_pag = document.getElementById("seguros-items-prataid");
+
+  function scrolling(event) {
+    if (window.scrollY >= 150) {
+      bikelist_ul.classList.add("opacity-1");
+    }
+    if (window.scrollY >= 1000) {
+      document
+        .querySelector(".tecnologia-imagem")
+        .classList.add("opacity-1-left");
+    }
+    if (window.scrollY >= 1950) {
+      document.querySelector(".parceiros h2").classList.add("opacity-1-s");
+    }
+      if (window.scrollY >= 3325) {
+        document.getElementById("seguros-items-prataid").classList.add("opacity-1-s");
+      }
+      if (window.scrollY >= 3325) {
+        document.getElementById("seguros-items-ouroid").classList.add("opacity-1-s");
+      }
+      if (window.scrollY >= 3066) {
+        document.getElementById("seguros-index").classList.add("opacity-1-s");
+      }
+      if (matchMedia("(max-width: 1250px)").matches){
+        document.getElementById("seguros-index").classList.remove('opacity-0');
+        document.getElementById("seguros-items-prataid").classList.remove('opacity-0');
+        document.getElementById("seguros-items-ouroid").classList.remove('opacity-0');
+      }
+  }
+
+  window.addEventListener("mousemove", att);
+  window.addEventListener("keypress", att);
+  function att(event) {
+    if (event.timeStamp >= 3000) {
+      bikelist_ul.classList.add("opacity-1");
+    }
+    if (event.timeStamp >= 6000) {
+      document
+        .querySelector(".tecnologia-imagem")
+        .classList.add("opacity-1-left");
+    }
+    if (event.timeStamp >= 10000) {
+      document.querySelector(".parceiros h2").classList.add("opacity-1-right");
+    }
+  }
+}
+//
 
